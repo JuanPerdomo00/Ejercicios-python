84664e4f5d61af3baf034a8172a2d092da0976f4
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-05-12 07:11:40 -0500

    segundo ejercicio

---
+++ ejercicio_2/respuesta.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def main():
+    '''Solucion 
+    
+    cadena = 'Hola Mundo'
+    imprimir cadena
+    '''
+
+    cadena = 'Hola Mundo'
+    print(cadena)
+
+if __name__ == '__main__':
+    main()
